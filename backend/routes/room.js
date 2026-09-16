1bd880b598d8db4b71d09e4571eff2ddb46d2919
Lokesh-Ramineni <lokesh.ramineni1@gmail.com>
2026-09-16 20:29:22 +0530

    added room by id

--- backend/routes/room.js
+++ backend/routes/room.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const authMiddleware = require("../middleware/authMiddleware");
-const { createRoom,getRoom,joinRoom ,leaveRoom} = require("../controllers/roomController");
+const { createRoom,getRoom,getRoomById,joinRoom ,leaveRoom} = require("../controllers/roomController");
 
 const router = express.Router();
 
 router.post("/createroom",authMiddleware,createRoom);
 router.post("/join",authMiddleware,joinRoom);
+router.get("/id/:id",authMiddleware,getRoomById);
 router.get("/:code",authMiddleware,getRoom)
 router.delete("/:code/leave", authMiddleware, leaveRoom);
 
